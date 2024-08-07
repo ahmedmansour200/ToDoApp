386957a0ed2549645b9407ef95e3749cd3ffe7cd
ahmedmansour200 <ahmedmansour10122000@gmail.com>
2024-08-07 15:05:31 +0300

    add all projact todo app ui

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <Text style={styles.title}>TODO APP</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Todo Title"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Todo Description"
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text>Add Todo</Text>
+      </TouchableOpacity>
+      <View style={styles.divider} />
+      <View style={styles.filterContainer}>
+        {['All', 'Active', 'Done'].map(status => (
+          <TouchableOpacity
+            key={status}
+          >
+            <Text>{status}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+    </View> 
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 20,
     backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginVertical: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginVertical: 10,
+    borderRadius: 5,
+  },
+  divider: {
+    height: 1,
+    backgroundColor: '#ccc',
+    marginVertical: 20,
+  },
+  filterContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  button: {
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#7C00FE',
+    padding: 10,
   },
 });
